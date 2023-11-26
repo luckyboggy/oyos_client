491a265f5b7e8b827cccbbf0e48ffeb8def2ce65
unknown <luckyboggy@gmail.com>
2023-11-26 12:51:17 +0500

    favorites refactor adapt

--- src/pages/Favorites/ui/Favorites.jsx
+++ src/pages/Favorites/ui/Favorites.jsx
@@ -13,60 +13,65 @@ import logo from "shared/assets/img/png/logo.png";
 const Favorites = observer(() => {
   const { user } = useContext(Context);
   const isSm = useScreenSize().isSm;
+  const basketNotEmpty =
+    user.favoritesItems.length !== 0 || user.localFavorites.length !== 0;
 
   useEffect(() => {}, [user.localFavorites, user.favoritesItems]);
 
   return (
     <div className={cls.favorites}>
-      {!user.isAuth && (
-        <div className={cls.unauthorized}>
-          <div className={cls.toAuth}>
-            <Text size={"m"} position={"center"} padding={"pv1"}>
-              Избранное
-            </Text>
-            {isSm ? (
-              <div>
-                <Link to="/login" className={cls.link}>
-                  <Text size={"s"} position={"center"} padding={"pv2"}>
-                    Войдите в аккаунт, чтобы смотреть избранное на любом
-                    устройстве >
-                  </Text>
-                </Link>
-
-                <div className={cls.logo}>
-                  <img src={logo} alt="logo" className={cls.logoImg} />
-                </div>
-                <Link to="/shop" className={cls.link}>
-                  <Text size={"s"} position={"center"} padding={"pv2"}>
-                    В магазин >
-                  </Text>
-                </Link>
+      <Text size={"m"} position={"center"} padding={"pv1"}>
+        Избранное
+      </Text>
+      {/* в авторизацию */}
+      {isSm ? (
+        <div className={cls.toAuth}>
+          {!user.isAuth && (
+            <Link to="/login" className={cls.link}>
+              <Text size={"s"} position={"center"} padding={"pv2"}>
+                Войдите в аккаунт, чтобы смотреть избранное на любом устройстве
+                >
+              </Text>
+            </Link>
+          )}
+          {!basketNotEmpty && (
+            <div>
+              <div className={cls.logo}>
+                <img src={logo} alt="logo" className={cls.logoImg} />
               </div>
-            ) : (
-              <div>
-                <Text size={"s"} position={"left"} padding={"pv2"}>
-                  Войдите в аккаунт, чтобы смотреть избранное на любом
-                  устройстве
-                </Text>
-                <Link to="/login">
-                  <CustomButton fontSize={"s"}>Войти</CustomButton>
-                </Link>
-              </div>
-            )}
-          </div>
+            </div>
+          )}
+        </div>
+      ) : (
+        <div className={cls.toAuth}>
+          {!user.isAuth && (
+            <div>
+              <Text size={"s"} position={"left"} padding={"pv2"}>
+                Войдите в аккаунт, чтобы смотреть избранное на любом устройстве
+              </Text>
+              <Link to="/login">
+                <CustomButton fontSize={"s"}>Войти</CustomButton>
+              </Link>
+            </div>
+          )}
         </div>
       )}
-
-      <div className={cls.favoritesList}>
-        {user.favoritesItems.length !== 0 ||
-        user.localFavorites.length !== 0 ? (
+      {/* в магазин */}
+      {basketNotEmpty ? (
+        <div className={cls.favoritesList}>
           <div className={cls.title}>Вам понравилось</div>
-        ) : (
+          <FavoritesList />
+        </div>
+      ) : (
+        <div className={cls.toShop}>
           <div className={cls.title}>Вы пока не нашли своё</div>
-        )}
-
-        <FavoritesList />
-      </div>
+          <Link to="/shop" className={cls.link}>
+            <Text size={"s"} position={"center"} padding={"pv2"}>
+              В магазин >
+            </Text>
+          </Link>
+        </div>
+      )}
     </div>
   );
 });
