9d42319188909f37e10230cab60b234408bda960
unknown <luckyboggy@gmail.com>
2024-01-29 22:51:43 +0500

    sidebar v2

--- src/widgets/SideBar/ui/SideBar.jsx
+++ src/widgets/SideBar/ui/SideBar.jsx
@@ -3,10 +3,21 @@ import cls from "./SideBar.module.scss";
 
 const SideBar = ({ options, selected, setSelected }) => {
   return (
-    <div className={cls.SideBar}>
+    <div className={cls.sidebar}>
+      <div className={cls.choosenItem}>
+        {options.find((option) => option.value === selected).name}
+      </div>
       <div className={cls.items}>
         {options.map((option) => (
-          <div className={cls.item}>{option.name}</div>
+          <div
+            className={cls.item}
+            key={option.name}
+            onClick={() => {
+              setSelected(option.value);
+            }}
+          >
+            {option.name}
+          </div>
         ))}
       </div>
     </div>
