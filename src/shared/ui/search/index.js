a9caa4f13512a42c66b0ee4e13ef74269aae2e55
unknown <luckyboggy@gmail.com>
2024-04-02 22:39:14 +0500

    deploy 0204_02

--- src/shared/ui/search/index.js
+++ src/shared/ui/search/index.js
@@ -1,5 +1,5 @@
 import { SearchInput } from "./searchInput/SearchInput";
 import { SearchVariants } from "./searchVariants/SearchVariants";
-import { SearchResult } from "./searchResult/SearchResult";
 
-export { SearchInput, SearchVariants, SearchResult };
+
+export { SearchInput, SearchVariants };
--- src/shared/ui/search/searchResult/searchResult.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import cls from "./SearchResult.module.scss";
-
-const SearchResult = () => {
-  return <div className={cls.searchResult}></div>;
-};
-export { SearchResult };
